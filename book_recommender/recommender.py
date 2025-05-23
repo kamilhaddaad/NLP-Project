7f9fdde7df6d396395791abddacbdace3f1abc61
ZChodorowska <121029279+M4agicBean@users.noreply.github.com>
2025-05-23 16:26:28 +0200

    Update

--- book_recommender/recommender.py
+++ book_recommender/recommender.py
@@ -8,20 +8,17 @@ book_embeddings = np.load('book_recommender/models/book_embeddings.npy')
 
 def recommend_books(book_title, num_recommendations=15):
     try:
-        # Get the index of the book (case-insensitive match)
         book_index = df[df['title'].str.lower() == book_title.lower()].index[0]
     except IndexError:
         print(f"Book with title '{book_title}' not found in the dataset.")
-        return pd.DataFrame(columns=["title", "summary"]) # Return empty DataFrame
+        return pd.DataFrame(columns=["title", "summary"])
 
-    # Get the embedding of the input book
     input_embedding = book_embeddings[book_index].reshape(1, -1)
 
     # Calculate cosine similarity between the input book and all other books
     similarities = cosine_similarity(input_embedding, book_embeddings).flatten()
 
     # Get the indices of the most similar books
-    # We exclude the first one because it will be the book itself
     similar_indices = similarities.argsort()[::-1][1:num_recommendations + 1]
 
     return df.iloc[similar_indices][["title", "summary"]]
